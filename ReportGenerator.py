445d2fc8cbfd2a47f3d3d8db026dd87f0ae82cc8
Austin Copley <acopley@mines.edu>
2022-07-07 20:57:23 -0500

    adds sheet for every unique asset id

--- ReportGenerator.py
+++ ReportGenerator.py
@@ -17,12 +17,36 @@ def main():
     except openpyxl.utils.exceptions.InvalidFileException:
         print(f"File: {xlsx} is not a valid excel file")
         exit()
-    sheet = wb.get_sheet_by_name('Total')
+    
+    # get the initial sheet from the original csv file
+    wsTotal = wb.active
+    heading = wsTotal[1]
 
-# delete G-J
+    # delete G-J, removing extraneous data that clients dont need to see
+    wsTotal.delete_cols(7, 4)
+
+    # create separate sheets:
+
+    # for each item in column B, if the assetID is new, create a new sheet
+    assets = []
+    for cell in wsTotal['B']:
+        if cell.value != "Asset ID":
+            if cell.value not in assets:
+                print(cell.value)
+                assets.append(cell.value)
+                wb.create_sheet(title=f"p{assets.index(cell.value)+1}")
+
+    wb.save(xlsx)
+
+    # add tab for vouchers and refunds
+    wb.create_sheet(title="vouchers", index=-1)
+    wb.create_sheet(title="refunds", index=-1)
+    
+
+
 # separate vouchers into separate sheet
 # separate refunds into separate sheet
-#sheet.delete_cols(7, 4)
+
 
 if __name__ == "__main__":
     main()
