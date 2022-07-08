6db1e719807bb1f2fa0bf787e6003259a69b8e38
Austin Copley <acopley@mines.edu>
2022-07-07 19:17:41 -0500

    fixed filenotfoundexception

--- ReportGenerator.py
+++ ReportGenerator.py
@@ -2,22 +2,22 @@ import openpyxl
 import pandas as pd
 
 
-
 def main():
     csv = 'transactions.csv'
     xlsx = "test.xlsx"
-    transactions = pd.read_csv(csv)
+    try:
+        transactions = pd.read_csv(csv)
+    except FileNotFoundError:
+        print(f"File: {xlsx} not found")
+        exit()
     transactions.to_excel(xlsx, sheet_name='Total', index=False)
 
     try:
         wb = openpyxl.load_workbook("./test.xlsx")
-        sheet = wb.get_sheet_by_name('Total')
-    except FileNotFoundError:
-        print(f"File: {xlsx} not found")
-        exit()
     except openpyxl.utils.exceptions.InvalidFileException:
         print(f"File: {xlsx} is not a valid excel file")
         exit()
+    sheet = wb.get_sheet_by_name('Total')
 
 # delete G-J
 # separate vouchers into separate sheet
